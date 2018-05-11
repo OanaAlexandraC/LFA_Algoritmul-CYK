5426f5609f8cede2d71c0c3679fb40f435c38830
OanaAlexandraC <36674400+OanaAlexandraC@users.noreply.github.com>
2018-05-11 15:42:23 +0300

    Add class

---
+++ algoritmCYK.h
@@ -0,0 +1,29 @@
+#ifndef LFA_ALGORITMUL_CYK_ALGORITMCYK_H
+#define LFA_ALGORITMUL_CYK_ALGORITMCYK_H
+
+#include <deque>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class AlgoritmCYK {
+    int numarProductii;
+    char **productii;
+    string cuvant;
+    deque<char> **matrice; //matrice triunghiulara rezultata in urma aplicarii algoritmului CYK
+public:
+    //AlgoritmCYK(); //constructor
+
+    //~AlgoritmCYK(); //destructor
+
+    //void afisareProductii();
+
+    //void completareMatrice();
+
+    //void afisareMatrice();
+
+    //bool acceptat();
+};
+
+#endif //LFA_ALGORITMUL_CYK_ALGORITMCYK_H
